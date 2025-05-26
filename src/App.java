78717147ae4861d59065810d4e6234495ed3def5
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-05-26 09:14:39 -0300

    Fazendo o menu do sistema de fichas

--- src/App.java
+++ src/App.java
@@ -4,16 +4,31 @@ public class App {
 
     public static void main(String[] args) {
 
-        Queue<String> minhaFila = new LinkedList<>();
-        minhaFila.offer("João");
-        minhaFila.offer("Maria");
-        minhaFila.offer("Pedro");
-        minhaFila.offer("Ana");
+        Scanner scanner = new Scanner(System.in);
+        Fila fila = new Fila();
 
-        System.out.println("Retorna o primeiro:"+minhaFila.peek());
+        while(true){
+            System.out.println("Escolha a opção:" +
+                    "\n1 - Nova ficha" +
+                    "\n2 - Ver próximo" +
+                    "\n3 - Chamar próximo" +
+                    "\n4 - Sair");
+            int escolha = scanner.nextInt();
+            scanner.nextLine();
+            switch(escolha){
+                case 1 ->{
+                    System.out.println("Informe o nome do cliente: ");
+                    String nomeCliente = scanner.nextLine();
+                    Ficha ficha = new Ficha(nomeCliente);
+                    fila.adicionar(ficha);
+                }
+                //TODO: Tratar mensagens de sucesso e erro
+                case 2 -> System.out.println(fila.inspecionar());
+                case 3 -> System.out.println(fila.chamarProximo());
+                case 4 -> System.exit(0);
+                default -> System.out.println("Opção Inválida");
+            }
 
-        while(!minhaFila.isEmpty()) {
-            System.out.println(minhaFila.poll());
         }
 
     }
