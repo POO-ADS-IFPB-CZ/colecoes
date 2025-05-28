696abe963cdc2cc0f888dddcdb36addfa4b8f74a
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-05-28 08:51:32 -0300

    TEstando os métodos

--- src/App.java
+++ src/App.java
@@ -4,7 +4,17 @@ public class App {
 
     public static void main(String[] args) {
 
+        PlayList playList = new PlayList("Minhas músicas");
 
+        playList.adicionarMusica(new Musica("Gostava tanto de você",
+                "Tim Maia", 1973));
+        playList.adicionarMusica(new Musica("Leviana",
+                "Zezo", 2000));
+        playList.adicionarMusica(new Musica("Nothing else matters",
+                "Metallica", 1991));
+
+        playList.ordenarPorAno();
+        System.out.println(playList);
 
     }
 
