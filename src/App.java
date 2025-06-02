795f89b37257415685df56aaca48fa0f195a4674
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-06-02 08:47:03 -0300

    Resolvendo a primeira questão

--- src/App.java
+++ src/App.java
@@ -6,16 +6,15 @@ public class App {
 
     public static void main(String[] args) {
 
-        Set<Pessoa> pessoas = new HashSet<>();
-        pessoas.add(new Pessoa("111.111.111-01",
-        "João", LocalDate.of(2000,6,12)));
-        pessoas.add(new Pessoa("333.333.333-03",
-        "Maria", LocalDate.of(1999,10,5)));
-        pessoas.add(new Pessoa("222.222.222-02",
-        "Ana", LocalDate.of(2010,6,2)));
+        String texto = "A casa branca estava cheia de flores rosas " +
+                "e a luz solar entrava pelas janelas iluminando " +
+                "cada cantinho um dia ensolarado perfeito para uma " +
+                "festa";
 
-        pessoas.stream().map(p -> p.getIdade())
-                .sorted().forEach(System.out::println);
+        Arrays.stream(texto.toUpperCase().split(" "))
+                .sorted()
+                .distinct()
+                .forEach(System.out::println);
 
     }
 
